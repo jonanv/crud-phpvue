3f9e8cc11e4a12f2fe77aead6409a9b35612ff06
Johanny Vargas González <jonan-vargas23@hotmail.com>
2021-05-28 16:23:09 -0500

    Se hace la implementacion del Create

--- api.php
+++ api.php
@@ -4,28 +4,44 @@
 
     // action = create, read, update, delete
     $action = "read";
-    $response = array("error"=>false);
-    
+    $response = array("error" => false);
+
     if (isset($_GET['action'])) {
         $action = $_GET['action'];
 
         switch ($action) {
-            case 'create':
-                echo "crear";
-                break;
-
             case 'read':
                 $u = $user->search("paisajes", "1");
                 if ($u) {
                     $response['paisajes'] = $u;
                     $response['message'] = "ok";
-                }
-                else {
+                } else {
                     $response['message'] = "Aun no hay registros";
                     $response['error'] = true;
                 }
                 break;
 
+            case 'create':
+                $nombre = $_POST['nombre'];
+                $descripcion = $_POST['descripcion'];
+                $foto = $_POST['foto']['name'];
+
+                $target_dir = "img/";
+                $target_file = $target_dir . basename($foto);
+                move_uploaded_file($_FILES['foto']['tmp_name'], $target_file);
+                // TODO: Revisar linea de move_file (31)
+
+                $data = "'" . $nombre . "','" . $descripcion . "','" . $foto . "'";
+                $u = $user->create("paisajes", $data);
+                if ($u) {
+                    $response['paisajes'] = $u;
+                    $response['message'] = "inserción exitosa";
+                } else {
+                    $response['message'] = "Aun no hay registros";
+                    $response['error'] = true;
+                }
+                break;
+                
             case 'update':
                 echo "modificar";
                 break;
@@ -33,7 +49,7 @@
             case 'delete':
                 echo "eliminar";
                 break;
-            
+
             default:
                 # code...
                 break;
