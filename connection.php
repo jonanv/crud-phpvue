2a9c22a9047b86b3a7afbd8b7625860c0611efc1
Johanny Vargas González <jonan-vargas23@hotmail.com>
2021-05-28 13:08:38 -0500

    Se crea archivo de conexion con las consultas

---
+++ connection.php
@@ -0,0 +1,47 @@
+<?php
+    class AppDB {
+        private $host = "localhost";
+        private $user = "root";
+        private $pass = "admin";
+        private $db = "crud-phpvue";
+
+        public $connection;
+        public function __construct() {
+            $this->connection = new mysqli($this->host, $this->user, $this->pass, $this->db)
+            or die(mysql_error());
+            $this->connection->set_charset("utf8");
+        }
+
+        // Insertar
+        public function create($table, $data) {
+            $response = $this->connection->query("INSERT INTO $table VALUES(null, $data)") or die($this->connection->error);
+            if ($response)
+                return true;
+            return false;
+        }
+
+        // Eliminar
+        public function delete($table, $condition) {
+            $response = $this->connection->query("DELETE FROM $table WHERE $condition") or die($this->connection->error);
+            if ($response)
+                return true;
+            return false;
+        }
+
+        // Actualizar
+        public function update($table, $fields, $condition) {
+            $response = $this->connection->query("UPDATE $table SET $fields WHERE $condition") or die($this->connection->error);
+            if ($response)
+                return true;
+            return false;
+        }
+
+        // Buscar
+        public function search($table, $condition) {
+            $response = $this->connection->query("SELECT * FROM $table WHERE $condition") or die($this->connection->error);
+            if ($response)
+                return $response->fetch_all(MYSQLI_ASSOC);
+            return false;
+        }
+    }
+    
